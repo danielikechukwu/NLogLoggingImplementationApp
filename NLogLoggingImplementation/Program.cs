997b314cd8ebbc20d47ba0fffefa608ed2f78fcc
Daniel ikechukwu <daniel.ikechukwu@avosoft.co.uk>
2025-07-24 20:51:45 +0100

    feat(program): Modify program file, removing previous logger and implementing NLogger.

--- NLogLoggingImplementation/Program.cs
+++ NLogLoggingImplementation/Program.cs
@@ -1,3 +1,5 @@
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Remove default logging providers to avoid duplicate logs.
+// Clears built-in providers (console, debug, etc.)
+builder.Logging.ClearProviders();
+
+// Set NLog as the logging provider for the application.
+// Configures the host to use NLog
+builder.Host.UseNLog();
 
 var app = builder.Build();
 
